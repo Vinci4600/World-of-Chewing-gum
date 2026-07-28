c4f7a7884276e7b13e9fd1e4aec9209f23ab0897
Vinci4600 <vincent.diergardt@gmail.com>
2026-07-28 23:37:00 +0200

    add code to Benutzer.java

--- Backend/src/main/java/org/example/backend/Model/Benutzer.java
+++ Backend/src/main/java/org/example/backend/Model/Benutzer.java
@@ -1,4 +1,61 @@
 package org.example.backend.Model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
-public class Benutzer {
-}
+    @Entity
+    @Inheritance(strategy = InheritanceType.JOINED)
+    public abstract class Benutzer {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String benutzername;
+        private String email;
+        private String passwort;
+
+        public Benutzer() {
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getBenutzername() {
+            return benutzername;
+        }
+
+        public void setBenutzername(String benutzername) {
+            this.benutzername = benutzername;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPasswort() {
+            return passwort;
+        }
+
+        public void setPasswort(String passwort) {
+            this.passwort = passwort;
+        }
+    }
+
+
+
+
+
+
