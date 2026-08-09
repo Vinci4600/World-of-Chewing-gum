90a3da8886f6d478c82092054475e5f846d3b3a1
Vinci4600 <vincent.diergardt@gmail.com>
2026-08-09 20:13:15 +0200

    Improved Benutzer.java

--- Backend/src/main/java/org/example/backend/Model/Benutzer.java
+++ Backend/src/main/java/org/example/backend/Model/Benutzer.java
@@ -1,4 +1,4 @@
-package org.example.backend.Model;
+package org.example.backend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
