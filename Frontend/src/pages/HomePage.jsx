77edf6ae4667ce609b744a737c7c52c39fcf6382
Vinci4600 <vincent.diergardt@gmail.com>
2026-08-20 10:14:26 +0200

    Create HomePage.jsx

---
+++ Frontend/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import {useState} from 'react'
+
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div className="Background">
+
+                <div className="Titel">World of Chewing gum</div>
+
+            </div>
+        </>
+    )
+}
+
+export default HomePage
