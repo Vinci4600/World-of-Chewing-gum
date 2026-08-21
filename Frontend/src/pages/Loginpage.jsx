c25ce73e92fceb89dc7deca1741bbd4c14416ad2
Vinci4600 <vincent.diergardt@gmail.com>
2026-08-21 14:08:06 +0200

    add style in Loginpage.jsx

---
+++ Frontend/src/pages/Loginpage.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react'
+
+import './components/Styles/Home.css'
+
+
+function Loginpage() {
+    const [count, setCount] = useState(0)
+
+    return (
+
+        <div className="Background-Intro">
+            <div className="Login-card">
+
+                <h1 className="he1">Login</h1>
+
+
+            </div>
+
+        </div>
+
+    );
+}
+
+export default Loginpage
