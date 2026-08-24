5724521ea0403762e0d2f1e823bf8c3f866961ca
Vinci4600 <vincent.diergardt@gmail.com>
2026-08-24 11:19:23 +0200

    add part one from create kaugummi

--- Frontend/src/pages/KaugummiAddPage.jsx
+++ Frontend/src/pages/KaugummiAddPage.jsx
@@ -3,26 +3,56 @@ import {useState} from 'react'
 import './components/Styles/Home.css'
 
 
-function KaugummiAddPage() {
+ function KaugummiAddPage() {
     const [kaugummi, setKaugummi] = useState([]);
     const [name, setName] = useState("");
     const [imageUrl, setImageUrl] = useState("");
-    const [marke, setMarket] = useState("");
+    const [marke, setMarke] = useState("");
     const [geschmack, setGeschmack] = useState("");
-    const [zuckerfrei, setZuckerfrei] = useState("");
+    const [zuckerfrei, setZuckerfrei] = useState(false);
     const [inhaltsstoffe, setInhaltsstoffe] = useState("");
 
-    return (
 
-        <div className="Background-Intro">
+    // Daten die ins Backend Geschickt werden
+    const kaugummiData = {
+        name: name,
+        imageUrl: imageUrl,
+        marke: marke,
+        geschmack: geschmack,
+        zuckerfrei: zuckerfrei,
+        inhaltsstoffe: inhaltsstoffe
+    };
+// Post Funktion
+     const kaugummiHinzufuegen = async () => {
+    try {
+        const response = await fetch(
+            "http://localhost:8080/api/kaugummi/add",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(kaugummiData)
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error("Kaugummi konnte nicht hinzugefügt werden");
+        }
+        const data = await response.json();
+
+        console.log("Erfolgreich hinzugefügt:", data);
 
 
+        return (
+
+            <div className="Background-Intro">
 
 
+            </div>
 
-        </div>
+        );
 
-    );
-}
 
-export default KaugummiAddPage
+        export default KaugummiAddPage;
+
