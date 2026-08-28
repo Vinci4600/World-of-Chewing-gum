ff128d56cb4c2869c4ad8c3f71965636b718c764
Vinci4600 <vincent.diergardt@gmail.com>
2026-08-28 11:09:35 +0200

    Add Style for navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,9 @@ import { Link, Routes, Route, useNavigate } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/Loginpage.jsx";
 import RegistrierungPage from "./pages/Registerpage.jsx";
-
+import lastpng from './pages/components/Bilder/Last.png'
 import KaugummiAddPage from "./pages/KaugummiAddPage.jsx";
+
 
 function App() {
     const [token, setToken] = useState(localStorage.getItem("token"));
@@ -17,26 +18,35 @@ function App() {
         navigate("/");
     };
 
+
     return (
         <div className="Background">
-            <nav>
-                <Link to="/">Home</Link>
 
-                {token ? (
-                    <>
-                        {" | "}
-                        <button onClick={handleLogout}>Logout</button>
-                    </>
-                ) : (
-                    <>
-                        {" | "}
-                        <Link to="/login">Login</Link>
-                        {" | "}
-                        <Link to="/register">Registrieren</Link>
-                        <Link to="/kaugummiadd">Kaugummi hinzufügen</Link>
+            <nav className="navbar">
+                <div className="navbar-logo">
+                    <Link to="/"><img className="navbar-logo img" src={lastpng} alt="WOC Logo"/></Link>
+                </div>
 
-                    </>
-                )}
+                <div className="navbar-links">
+
+
+                    {token ? (
+                        <>
+                            <Link to="/kaugummiadd">
+                                Kaugummi hinzufügen
+                            </Link>
+
+                            <button onClick={handleLogout}>
+                                Logout
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Registrieren</Link>
+                        </>
+                    )}
+                </div>
             </nav>
 
             <div className="content">
@@ -45,10 +55,11 @@ function App() {
                     <Route path="/login" element={<LoginPage setToken={setToken} />} />
                     <Route path="/register" element={<RegistrierungPage />} />
                     <Route path="/kaugummiadd" element={<KaugummiAddPage />} />
-
                 </Routes>
             </div>
+
         </div>
+
     );
 }
 
