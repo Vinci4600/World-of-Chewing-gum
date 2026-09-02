99e187dc4c455baa45cd5277f96cbccdc82e4901
elias4864 <172511171+elias4864@users.noreply.github.com>
2026-09-02 09:25:08 +0200

    Added Logo on Navbar

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/Loginpage.jsx";
+import RegistrierungPage from "./pages/Registerpage.jsx";
+import Chewinggum from "../public/Last.png";
+import KaugummiAddPage from "./pages/KaugummiAddPage.jsx";
+
+function App() {
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        setToken(null);
+        navigate("/");
+    };
+
+    return (
+        <div className="Background">
+            <nav>
+                <Link to="/">Home</Link>
+                <img src={Chewinggum} alt="Chewinggum" className="navbar-logo"/>
+
+
+                {token ? (
+                    <>
+                        {" | "}
+                        <button onClick={handleLogout}>Logout</button>
+                    </>
+                ) : (
+                    <>
+                        {" | "}
+                        <Link to="/login">Login</Link>
+                        {" | "}
+                        <Link to="/register">Registrieren</Link>
+                        <Link to="/kaugummiadd">Kaugummi hinzufügen</Link>
+
+                    </>
+                )}
+            </nav>
+
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage setToken={setToken} />} />
+                    <Route path="/register" element={<RegistrierungPage />} />
+                    <Route path="/kaugummiadd" element={<KaugummiAddPage />} />
+
+                </Routes>
+            </div>
+        </div>
+    );
+}
+
+export default App;
