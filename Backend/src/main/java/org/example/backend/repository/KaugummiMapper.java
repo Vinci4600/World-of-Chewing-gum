201ac7ea004cd00c8bd22f070902cc1cf9d519fe
elias4864 <172511171+elias4864@users.noreply.github.com>
2026-09-03 11:32:39 +0200

    Changes Pom.xm and Kagummi Mapper

--- Backend/src/main/java/org/example/backend/repository/KaugummiMapper.java
+++ Backend/src/main/java/org/example/backend/repository/KaugummiMapper.java
@@ -1,5 +1,9 @@
 package org.example.backend.repository;
+import org.example.backend.Model.Kaugummi;
+import org.example.backend.dto.KaugummiDTO;
 import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring") // componentModel="spring" ist wichtig für Dependency Injection
 public interface KaugummiMapper {
+
+    KaugummiDTO toDo(Kaugummi editierterKaugummis);
 }
