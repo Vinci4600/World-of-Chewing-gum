b555232421c8e30090334827fa7775f09200955a
elias4864 <172511171+elias4864@users.noreply.github.com>
2026-09-08 00:12:56 +0200

    Fixed Cors Axios

--- Backend/src/main/java/org/example/backend/config/SecurityConfig.java
+++ Backend/src/main/java/org/example/backend/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> {})
                 .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers(
