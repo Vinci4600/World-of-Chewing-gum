5a854fdb808f3b9bae6fcf1dfd82817074b290b4
Vinci4600 <vincent.diergardt@gmail.com>
2026-09-10 12:08:09 +0200

    <replace KaugummiPage.jsx and KaugummiDetailPage in the open route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -51,12 +51,14 @@ function App() {
                     {/* Öffentliche Routen */}
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegistrierungPage />} />
+                    <Route path="/kaugummiPage" element={<KaugummiPage />} />
+                    <Route path="/kaugummi/:id" element={<KaugummiDetailPage />} />
 
                     {/* Geschützte Routen (Nicht eingeloggt -> Redirect zu /login) */}
                     <Route element={<ProtectedRoute />}>
                         <Route path="/" element={<HomePage />} />
-                        <Route path="/kaugummiPage" element={<KaugummiPage />} />
-                        <Route path="/kaugummi/:id" element={<KaugummiDetailPage />} />
+
+
                         <Route path="/kaugummiadd" element={<KaugummiAddPage />} />
                         <Route path="/kaugummiedit/:id" element={<KaugummiEditPage />} />
                     </Route>
